4319fe34018a907a006f6e3fcfd52911d31887f6
hpeter19 <njoshpitar@gmail.com>
2025-12-21 10:40:34 +0300

    adding images

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState,useRef,useEffect } from 'react'
 import {Chatbot} from 'supersimpledev';
+import RobotProfileImage from './assets/robot.png';
+import UserProfileImage from './assets/user.png';
 import './App.css'
 
  function ChatInput({chatMessages,setChatMessages}){
@@ -65,11 +67,11 @@ import './App.css'
         */
       return(
         <div className={sender=== 'user'? 'chat-message-user' : 'chat-message-operator'}>
-          {sender==='robot' &&  <img src="robot.png" className="chat-message-profile" /> }
+          {sender==='robot' &&  <img src={RobotProfileImage} className="chat-message-profile" /> }
           <div className="chat-message-text">
            {message}
           </div>
-          {sender==='user' && <img src="user.png" className="chat-message-profile" />}
+          {sender==='user' && <img src={UserProfileImage} className="chat-message-profile" />}
         </div>
       );
     }
