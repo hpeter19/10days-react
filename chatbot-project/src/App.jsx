b33bcfe6edd7938579c0dc7533eb7b459a767fa4
hpeter19 <njoshpitar@gmail.com>
2025-12-21 11:06:05 +0300

    setting up chat messages component

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,37 +1,7 @@
-import { useState,useRef,useEffect } from 'react'
+import { useState} from 'react'
 import { ChatInput } from './Components/ChatInput';
-import { ChatMessage } from './Components/ChatMessage';
+import ChatMessages from './Components/ChatMessages';
 import './App.css'
-
-
-
-    function ChatMessages({chatMessages}){
-      const chatMessagesRef= useRef(null);
-
-      useEffect(()=>{
-        const containerElem=chatMessagesRef.current
-        if(containerElem){
-          containerElem.scrollTop=containerElem.scrollHeight
-        }
-
-      },[chatMessages]);
-
-
-        return(
-        <div className="chat-messages-container" ref={chatMessagesRef}>
-             {chatMessages.map((chatMessage)=>{
-              return(
-                <ChatMessage
-                  message={chatMessage.message}
-                  sender={chatMessage.sender}
-                  key={chatMessage.id}
-                />
-              );
-            })}
-          </div>
-         );
-    } 
-
 
 function App(){
 
