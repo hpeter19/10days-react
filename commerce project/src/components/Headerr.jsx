20985811aba90a0ca473646024fb7f9abd607620
hpeter19 <njoshpitar@gmail.com>
2025-12-29 19:38:01 +0300

    div for

--- commerce project/src/components/Headerr.jsx
+++ commerce project/src/components/Headerr.jsx
@@ -1,15 +1,13 @@
-import {Link} from 'react-router';
-import "../components/header.css";
+import "../components/headerr.css";
 
-
-export function Header() {
+export function Headerr() {
   return (
     <div className="header">
       <div className="left-section">
-        <Link to="/" className="header-link">
+        <a href="/" className="header-link">
           <img className="logo" src="images/logo-white.png" />
           <img className="mobile-logo" src="images/mobile-logo-white.png" />
-        </Link>
+        </a>
       </div>
 
       <div className="middle-section">
@@ -21,16 +19,15 @@ export function Header() {
       </div>
 
       <div className="right-section">
-        <Link className="orders-link header-link" to="/orders">
+        <a className="orders-link header-link" href="/orders">
           <span className="orders-text">Orders</span>
-        </Link>
+        </a>
 
-        <Link className="cart-link header-link" to="/checkout">
+        <a className="cart-link header-link" href="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">3</div>
           <div className="cart-text">Cart</div>
-          <div className=''></div>
-        </Link>
+        </a>
       </div>
     </div>
   );
--- commerce project/src/pages/HomePage.jsx
+++ commerce project/src/pages/HomePage.jsx
@@ -2,7 +2,15 @@ import { Header } from "../components/Header";
 import "./HomePage.css";
 import "../../starting-code/data/products";
 import { products } from "../../starting-code/data/products";
+
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response)=>{
+    return  response.json().then((data)=>{
+          console.log(data)
+      })
+  })
+
   return (
     <>
       <Header />
